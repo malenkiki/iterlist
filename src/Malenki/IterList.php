0c80df7b9654c3ea219baec83a5171bbc28b6ee1
malenkiki <petit.michel@gmail.com>
2013-07-22 13:09:41 +0200

    White spaces issues

--- src/Malenki/IterList.php
+++ src/Malenki/IterList.php
@@ -4,7 +4,7 @@ namespace Malenki;
 
 class IterList implements Iterator, Countable, ArrayAccess
 {
-	private $int_position = 0;
+    private $int_position = 0;
 
     private $int_count = 0;
 
@@ -14,63 +14,63 @@ class IterList implements Iterator, Countable, ArrayAccess
 
     public function __construct($arr = null)
     {
-		$this->int_position = 0;
+        $this->int_position = 0;
 
         if(is_array($arr))
         {
-			$this->arr_items = $arr;
-			$this->int_count = count($this->arr_items);
-		}
-	}
+            $this->arr_items = $arr;
+            $this->int_count = count($this->arr_items);
+        }
+    }
 
 
 
     public function rewind()
     {
-		$this->int_position = 0;
-	}
+        $this->int_position = 0;
+    }
 
 
 
     public function current()
     {
-		return $this->arr_items[$this->int_position];
-	}
+        return $this->arr_items[$this->int_position];
+    }
 
 
 
     public function key()
     {
-		return $this->int_position;
-	}
+        return $this->int_position;
+    }
 
 
 
     public function next()
     {
-		++$this->int_position;
-	}
+        ++$this->int_position;
+    }
 
 
 
     public function valid()
     {
-		return isset($this->arr_items[$this->int_position]);
-	}
+        return isset($this->arr_items[$this->int_position]);
+    }
 
 
 
     public function count()
     {
-		return $this->int_count;
-	}
+        return $this->int_count;
+    }
 
 
 
     public function getArrayCopy()
     {
-		return $this->arr_items;
-	}
+        return $this->arr_items;
+    }
 
 
 
@@ -78,49 +78,49 @@ class IterList implements Iterator, Countable, ArrayAccess
     {
         if (is_null($offset))
         {
-			$this->arr_items[] = $value;
-		}
+            $this->arr_items[] = $value;
+        }
         else
         {
-			$this->arr_items[$offset] = $value;
-		}
-	}
+            $this->arr_items[$offset] = $value;
+        }
+    }
 
 
 
     public function offsetExists($offset)
     {
-		return isset($this->arr_items[$offset]);
-	}
+        return isset($this->arr_items[$offset]);
+    }
 
 
 
     public function offsetUnset($offset)
     {
-		unset($this->arr_items[$offset]);
-	}
+        unset($this->arr_items[$offset]);
+    }
 
 
 
     public function offsetGet($offset)
     {
-		return isset($this->arr_items[$offset]) ? $this->arr_items[$offset] : null;
-	}
+        return isset($this->arr_items[$offset]) ? $this->arr_items[$offset] : null;
+    }
 
 
 
     public function add($item)
     {
-		$this->arr_items[] = $item;
-		$this->int_count = count($this->arr_items);
-	}
+        $this->arr_items[] = $item;
+        $this->int_count = count($this->arr_items);
+    }
 
 
 
     public function first()
     {
-		return $this->int_position == 0;
-	}
+        return $this->int_position == 0;
+    }
 
 
 
@@ -128,31 +128,31 @@ class IterList implements Iterator, Countable, ArrayAccess
     {
         if($this->count() > 1)
         {
-			return $this->int_position == $this->count() - 2;
-		}
-        
+            return $this->int_position == $this->count() - 2;
+        }
+
         return false;
-	}
+    }
 
 
 
     public function void()
     {
-		return $this->count() == 0;
-	}
+        return $this->count() == 0;
+    }
 
 
 
     public function hasOnlyOne()
     {
-		return $this->count() == 1;
-	}
+        return $this->count() == 1;
+    }
 
 
 
     public function last()
     {
-		return $this->int_position == $this->count() - 1;
-	}
+        return $this->int_position == $this->count() - 1;
+    }
 }
 
